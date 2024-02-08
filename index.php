0e68311f2fe3462bd71a4e078abcfd363ca65949
Michail Spinelli <michailspinelli96@gmail.com>
2024-02-08 17:40:58 +0100

    inizio

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <?php
+        /*
+            Descrizione
+            Dobbiamo creare una web-app che permetta di leggere una lista di Todo.
+            Deve essere anche gestita la persistenza dei dati leggendoli da un file JSON.
+            Stack
+            Html, CSS, VueJS (importato tramite CDN), axios, PHP
+            Consigli
+            Nello svolgere l'esercizio seguite un approccio graduale.
+            Prima assicuratevi che la vostra pagina index.php/index.html (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre "API").
+            Solo a questo punto sarà utile passare alla lettura della lista da un file JSON.
+        */
+    ?>
+</body>
+</html>
